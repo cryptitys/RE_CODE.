399f14f35d62f8e791d5503ae441d2054e8eca5b
vitor2008s969 <vrodriguespereira11@gmail.com>
2025-06-19 11:18:47 -0300

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function copyCode(button) {
-    const code = button.previousElementSibling.textContent;
-    navigator.clipboard.writeText(code);
-    button.textContent = "Copiado!";
-    setTimeout(() => button.textContent = "Copiar", 2000);
-}
